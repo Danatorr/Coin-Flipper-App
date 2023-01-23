65f8162696f17bb520e18006a982020235f01bf8
Danatorr <daniel.torres.developer@gmail.com>
2023-01-23 17:02:47 -0300

    Initial Commit

---
+++ Coin-Flip-App/Coin-Flip-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coin-Flip-App
+//
+//  Created by Daniel Torres on 23/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
