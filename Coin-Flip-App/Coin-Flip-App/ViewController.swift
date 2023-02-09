7365ba05bb78c66f3a2c14bc97378d2db7b9d78b
Torres <daniel.torres.developer@gmail.com>
2023-02-09 17:06:23 -0300

    Language Change Button Added

--- Coin-Flip-App/Coin-Flip-App/ViewController.swift
+++ Coin-Flip-App/Coin-Flip-App/ViewController.swift
@@ -11,8 +11,13 @@ import AVFoundation
 class ViewController: UIViewController {
     var player: AVAudioPlayer?
     
+    @IBOutlet weak var languageImage: UIButton!
     @IBOutlet weak var coinImage: UIImageView!
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var flipButton: UIButton!
+    
+    var result = 0
+    var language = "en"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +25,69 @@ class ViewController: UIViewController {
     }
 
     @IBAction func ButtonPress(_ sender: UIButton) {
-        let result = Int.random(in: 1..<3)
+        let pressedButton = sender.tag
+        
+        switch pressedButton {
+            case 1:
+                flip()
+            case 2:
+                changeLanguage()
+            default:
+                break
+        }
+    }
+    
+    func changeLanguage(){
+        if(language == "en"){
+            languageImage.setImage(UIImage(named: "USAFlag"), for: .normal)
+            flipButton.setTitle("FLIP", for: .normal)
+        } else if(language == "pt"){
+            languageImage.setImage(UIImage(named: "BrazilFlag"), for: .normal)
+            flipButton.setTitle("JOGAR", for: .normal)
+        }
+
+        
+        if(language == "en"){
+            language = "pt"
+        } else if(language == "pt"){
+            language = "en"
+        }
+        
+        if(language == "en" && result == 1){
+            coinImage.image = UIImage(named: "Heads")
+            label.text = "HEADS"
+        } else if(language == "en" && result == 2){
+            coinImage.image = UIImage(named: "Tails")
+            label.text = "TAILS"
+        } else if(language == "pt" && result == 1){
+            coinImage.image = UIImage(named: "Cara")
+            label.text = "CARA"
+        } else if(language == "pt" && result == 2){
+            coinImage.image = UIImage(named: "Coroa")
+            label.text = "COROA"
+        } else{
+            label.text = "???"
+        }
+    }
+    
+    func flip(){
+        result = Int.random(in: 1..<3)
         
         playSound()
         
-        if(result == 1){
+        if(language == "en" && result == 1){
             coinImage.image = UIImage(named: "Heads")
             label.text = "HEADS"
-        }
-        else if(result == 2){
+        } else if(language == "en" && result == 2){
             coinImage.image = UIImage(named: "Tails")
             label.text = "TAILS"
-        }
-        else{
+        } else if(language == "pt" && result == 1){
+            coinImage.image = UIImage(named: "Cara")
+            label.text = "CARA"
+        } else if(language == "pt" && result == 2){
+            coinImage.image = UIImage(named: "Coroa")
+            label.text = "COROA"
+        } else{
             label.text = "???"
         }
     }
@@ -48,7 +103,8 @@ class ViewController: UIViewController {
             
         } catch let error {
             print(error.localizedDescription)
-        }    }
+        }
+    }
 
 }
 
